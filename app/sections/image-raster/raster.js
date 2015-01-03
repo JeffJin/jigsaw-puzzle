6f5720e9bcc2c8ff9b94a97eb7d4fca6e95769c6
Jeff Jin <jeff@jeffjin.com>
2015-01-03 04:03:19 -0500

    raster image added

--- app/sections/image-raster/raster.js
+++ app/sections/image-raster/raster.js
@@ -13,7 +13,7 @@ angular.module('jigsawPuzzleApp')
     function getTileRaster(sourceRaster, x, y, width, height) {
       var targetRaster = new paper.Raster('empty-image');
       var data = sourceRaster.getData(new paper.Rectangle(x, y, width, height));
-      targetRaster.setData(data, new paper.Point(10, 25))
+      targetRaster.setData(data, new paper.Point(10, 25));
       targetRaster.position = new paper.Point(150, 125);
       return targetRaster;
     }
@@ -27,15 +27,28 @@ angular.module('jigsawPuzzleApp')
 
       var puzzleImage = new paper.Raster('raster-image');
       puzzleImage.position = new paper.Point(400, 300);
-      //var image = getTileRaster(puzzleImage, 150, 125, 395, 285);
-      //
-      //var border = boxPath.clone();
-      //border.strokeColor = '#000';
-      //border.strokeWidth = 1;
-      //
-      //var tile = new paper.Group(curve, border, image, border);
-      //tile.clipped = true;
-      //tile.opacity = 1;
+
+      //var raster = getTileRaster(puzzleImage, 150, 125, 320, 320);
+      puzzleImage.scale(0.5);
+
+      // Rotate the raster by 45 degrees:
+      puzzleImage.rotate(45);
+
+      var circle = new paper.Path.Circle({
+        center: [80, 50],
+        radius: 5,
+        fillColor: 'red'
+      });
+
+      // Create a rasterized version of the path:
+      var raster = circle.rasterize();
+
+      // Move it 100pt to the right:
+      raster.position.x += 100;
+
+      // Scale the path and the raster by 300%, so we can compare them:
+      circle.scale(5);
+      raster.scale(5);
 
       // Draw the view now:
       //paper.view.draw();
